07c41a5e18ae729981088c30e2ecbd6437674218
Ovidijus <ovidijus.e@gmail.com>
2024-03-06 18:09:17 +0200

    exam css changes

--- react_praktine_uzduotis/src/App.jsx
+++ react_praktine_uzduotis/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState, useEffect } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Header from "./Components/Header/Header";
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import { ClipLoader } from "react-spinners";
-import { getAllData } from "./Components/services/get";
-import CardList from "./Components/CardList/CardList";
-import CardDetails from "./Components/CardDetails/CardDetails";
+import { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './Components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import { ClipLoader } from 'react-spinners';
+import { getAllData } from './Components/services/get';
+import CardList from './Components/CardList/CardList';
+import CardDetails from './Components/CardDetails/CardDetails';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   const [donors, setDonors] = useState([]);
@@ -34,19 +35,19 @@ function App() {
 
   return (
     <>
-      <div className="App">
+      <div className='App'>
         <ToastContainer autoClose={1500} />
         <Header setUpdate={setUpdate} />
         <Routes>
           <Route index element={<HomePage />} />
-          <Route path="register" element={<RegistrationPage setUpdate={setUpdate} />} />
+          <Route path='register' element={<RegistrationPage setUpdate={setUpdate} />} />
           <Route
-            path="donors-list"
+            path='donors-list'
             element={
               !isLoading && donors.length > 0 ? (
                 <CardList donors={donors} setUpdate={setUpdate} />
               ) : (
-                <div className="loading">
+                <div className='loading'>
                   {donors.length === 0 ? (
                     <h2>No donors, found...</h2>
                   ) : (
@@ -59,9 +60,10 @@ function App() {
               )
             }
           />
-          <Route path="donor-details/:id" element={<CardDetails setUpdate={setUpdate} />} />
-          <Route path="*" element={<NotFoundPage />} />
+          <Route path='donor-details/:id' element={<CardDetails setUpdate={setUpdate} />} />
+          <Route path='*' element={<NotFoundPage />} />
         </Routes>
+        <Footer />
       </div>
     </>
   );
